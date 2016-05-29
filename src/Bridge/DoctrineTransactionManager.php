f5b602d731218a3e946a84417ed4f4b51c33190d
Fabien MEURILLON <fabien@meurillon.org>
2016-05-29 14:53:44 +0200

    Add interface documentation

---
+++ src/Bridge/DoctrineTransactionManager.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace FabienM\TransactionManager\Bridge;
+
+class DoctrineTransactionManager
+{
+
+}
--- src/Core/TransactionManagerInterface.php
+++ src/Core/TransactionManagerInterface.php
@@ -8,18 +8,26 @@ interface TransactionManagerInterface
 {
     /**
      * Commit the current transaction.
+     *
+     * If the transaction has been started with `readOnly` turned on, a rollback MUST be performed
+     *
      * @throws UnsupportedMethodException
      */
     public function commit();
 
     /**
      * Rollback the current transaction.
+     *
      * @throws UnsupportedMethodException
      */
     public function rollback();
 
     /**
      * Start a new transaction.
+     *
+     * If a transaction has already been started and the underlying DBMS supports savepoint, a savepoint SHOULD be
+     * created instead.
+     *
      * @param bool $readOnly mark this transaction as read only
      * @throws UnsupportedMethodException
      */
